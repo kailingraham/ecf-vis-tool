5e09eb2efcc66274ce4b9abc538b28eb1f119bce
kailingraham <kailingraham@gmail.com>
2023-07-05 19:15:28 +1000

    Adding dual handle filter slider

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,39 +1,40 @@
 <script>
-    import { Modals, closeModal } from "svelte-modals";
+  import "../app.postcss";
+  import { Modals, closeModal } from "svelte-modals";
 </script>
 
 <nav class="navbar">
-    <h1 class="title">
-        Migrant Employment & the Energy Transition (MEET)
-    </h1>
+  <h1 class="title">
+    Employment Vulnerability to the Energy Transition (E-VET) tool
+  </h1>
 </nav>
 
 <slot />
 
 <Modals>
-    <div slot="backdrop" class="backdrop" on:click={closeModal} />
+  <div slot="backdrop" class="backdrop" on:click={closeModal} />
 </Modals>
 
 <style>
-    .navbar {
-        display: flex;
-        align-items: center;
-        justify-content: center; /* Center horizontally */
-        height: 50px;
-        background-color: white;
-    }
+  .navbar {
+    display: flex;
+    align-items: center;
+    justify-content: center; /* Center horizontally */
+    height: 50px;
+    background-color: white;
+  }
 
-    .title {
-        font-weight: bold;
-        font-family: "Cardo", serif;
-    }
+  .title {
+    font-weight: bold;
+    font-family: "Cardo", serif;
+  }
 
-    .backdrop {
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        right: 0;
-        left: 0;
-        background: rgba(0, 0, 0, 0.5);
-    }
+  .backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.5);
+  }
 </style>
